1ffe9d1a09017ed31a5cf65f589ce530ff29bc31
Abgal <hmdabgal@gmail.com>
2026-06-25 23:18:40 +0600

    delet funtion add

--- index.js
+++ index.js
@@ -756,6 +756,26 @@ app.get("/admin/stats", verifySessionToken, async (req, res) => {
   }
 });
 
+
+
+app.delete22("/bookmarks/:id",verifySessionToken,  async (req, res) => {
+  try {
+    const result = await bookmarksCollection.deleteOne({
+      _id: new ObjectId(req.params.id),
+    });
+
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({
+      message: "Failed to remove bookmark",
+    });
+  }
+});
+
+
+
+
+
  
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
